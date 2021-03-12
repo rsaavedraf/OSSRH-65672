f881dbaa2da3d43ce127effd3e9fb1696d2951ab
rsaavedraf <raul.saavedra@gmail.com>
2021-03-12 14:15:55 +0100

    After cloning the ossrh demo repo

---
+++ src/main/java/com/simplgility/training/App.java
@@ -0,0 +1,14 @@
+package com.simplgility.training;
+
+/**
+ * Hello world!
+ * Cloned to test deployment to Maven Central
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
